59520a994c3a990307413e2436017d9b60505718
bruno coelho <brunocoelho@luizalabs.com>
2023-08-16 19:12:59 -0300

    Service user

---
+++ produtos/service.py
@@ -0,0 +1,30 @@
+from models import models as md
+from database import db_session as db
+from sqlalchemy.future import select
+from sqlalchemy import delete
+
+# Usuario
+class userService:
+    async def create_user(name_user:str, email:str):
+        async with db.async_session() as session:
+            session.add(md.User(name_user=name_user, email=email)) 
+            await session.commit()
+    
+    async def delete_user(id_user: int):
+        async with db.async_session() as session:
+            await session.execut(delete(md.User).where(md.User.id_user==id_user))
+            await session.commit()
+
+    async def list_user():
+        async with db.async_session() as session:
+            result = await session.execute(select(md.User))
+            return result.scalars().all()
+        
+    async def get_by_id(id_user: int):
+        async with db.async_session() as session:
+            result = await session.execute(select(md.User).where(md.User.id_user==id_user))
+            return result.scalar()
+    
+# Produto
+
+# Favoritos
