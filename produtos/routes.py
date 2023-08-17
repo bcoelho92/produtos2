28e122644b2cc44d25eba12530ac9ef52d11e79d
bruno coelho <brunocoelho@luizalabs.com>
2023-08-17 09:28:09 -0300

    rotas product

--- produtos/routes.py
+++ produtos/routes.py
@@ -9,7 +9,7 @@ from database.schemas import (
     ProductsSchema, RequestProducts, RequestIdProducts,
     ErrorOutput, StandardOutput, UserListOutput
 )
-from service import UserService, FavoriteService
+from service import UserService, ProductService
 
 router_user = APIRouter()
 router_favorites = APIRouter()
@@ -32,10 +32,21 @@ async def user_list():
         raise HTTPException(400, detail=str(error))
 
 @router_user.delete('/delete')
-async def list_users():
+async def users_delete():
     pass
 
 # PRODUCT ROUTERS
-@router_user.get('/list')
-async def list_users():
+@router_products.post('/create', description='create product!', response_model=StandardOutput, responses={400: {'model': ErrorOutput}})
+async def product_create():
+    pass
+
+@router_products.get('/list', response_model=List[UserListOutput], responses={400: {'model': ErrorOutput}})
+async def product_list():
+    try:
+        return await UserService.list_user()
+    except Exception as error:
+        raise HTTPException(400, detail=str(error))
+
+@router_products.delete('/delete')
+async def product_delete():
     pass
