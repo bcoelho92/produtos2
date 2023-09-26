d1d0f20ba6c25954e326e7492a8b81fe159bf41c
bruno coelho <brunocoelho@luizalabs.com>
2023-09-26 09:04:42 -0300

    ajustes

--- produtos/query.py
+++ produtos/query.py
@@ -30,6 +30,11 @@ class Queries:
             async with db.async_session() as session:
                 result = await session.execute(select(md.Product).where(md.Product.title==titulo))
                 return result.scalar()
+    
+    async def get_id_product_by_title(titulo: str):
+            async with db.async_session() as session:
+                result = await session.query(md.Product.id_product).filter(md.Product.title==titulo).first()
+                return result.scalar()      
 
     # async def get_favorite_by_ids(id_user: int, id_product: int):
     #     async with db.async_session() as session:
--- produtos/tests/test_sample.py
+++ produtos/tests/test_sample.py
@@ -7,52 +7,52 @@ import time
 client = TestClient(app)
 tm = 3
 
-# @pytest.mark.asyncio
-# async def test_get_root():
-#     response = client.get('/')
-#     assert response.status_code == 200
-#     assert response.json() == {"message": "API favoritos!"}
+@pytest.mark.asyncio
+async def test_get_root():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == {"message": "API favoritos!"}
     
-# @pytest.mark.asyncio
-# async def test_user_create_correto():
-#     response = client.post(
-#         '/users/create/',
-#         json={"name_user": "test", "email": "test@test.com"},
-#     )
-#     assert response.status_code == 201
-#     assert response.json() == {
-#         "message":"Cadastrado com sucesso!"
-#     } 
+@pytest.mark.asyncio
+async def test_user_create_correto():
+    response = client.post(
+        '/users/create/',
+        json={"name_user": "test", "email": "test@test.com"},
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        "message":"Cadastrado com sucesso!"
+    } 
 
-# @pytest.mark.asyncio
-# async def test_user_create_validacao_email():
-#     response = client.post(
-#         "/users/create/",
-#         json={"name_user": "test", "email": "testaaatest.com"},
-#     )
-#     assert response.status_code == 422
-#     assert response.json() == {
-#     "detail": [
-#         {
-#         "loc": [
-#             "body",
-#             "email"
-#         ],
-#         "msg": "value is not a valid email address",
-#         "type": "value_error.email"
-#         }
-#     ]
-#     }
+@pytest.mark.asyncio
+async def test_user_create_validacao_email():
+    response = client.post(
+        "/users/create/",
+        json={"name_user": "test", "email": "testaaatest.com"},
+    )
+    assert response.status_code == 422
+    assert response.json() == {
+    "detail": [
+        {
+        "loc": [
+            "body",
+            "email"
+        ],
+        "msg": "value is not a valid email address",
+        "type": "value_error.email"
+        }
+    ]
+    }
 
-# # time.sleep(tm)
+# time.sleep(tm)
 
-# @pytest.mark.asyncio
-# async def test_user_delete():
-#     email = "test@test.com"
-#     response = client.delete(
-#         url=f"/users/{email}"
-#     )
-#     assert response.status_code == 204
+@pytest.mark.asyncio
+async def test_user_delete():
+    email = "test@test.com"
+    response = client.delete(
+        url=f"/users/{email}"
+    )
+    assert response.status_code == 204
 
 # PRODUTOS 
 
